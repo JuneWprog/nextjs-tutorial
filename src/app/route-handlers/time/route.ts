e2086a5f7a46a833880450159426b9c93c31615f
Jun wang <wang.jun6@northeastern.edu>
2025-04-10 23:58:02 -0600

    caching

---
+++ src/app/route-handlers/time/route.ts
@@ -0,0 +1,16 @@
+export const dynamic = "force-static";
+export const revalidate = 10;
+
+
+//export const dynamic = "force-static"; This tells Next.js to statically generate the page or API route at build time.
+
+/**
+ * 
+ * The first request builds the response and caches it.
+ * For the next 10 seconds, anyone who hits that endpoint gets the same cached time.
+ * After 10 seconds, a new version is built in the background, and that becomes the new cached version for the next 10 seconds.
+ */
+
+export async function GET() {
+  return Response.json({ time: new Date().toLocaleTimeString() });
+}
